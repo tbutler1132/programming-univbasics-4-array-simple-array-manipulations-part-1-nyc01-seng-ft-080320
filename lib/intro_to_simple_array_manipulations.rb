f5176654386925e6829fb55d6f6b0facc54ddc6c
artificial-composition-7121 <tbutler1132@gmail.com>
2020-07-20 16:05:21 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,5 +3,5 @@ def using_push(colors_in_the_rainbow, next_color)
 end
 
 def using_unshift(bouroughs_in_nyc, new_neighborhood)
-  bouroughs_in_nyc.unshift("Ear")
+  bouroughs_in_nyc.unshift("Staten Island")
 end
