4ce0e9216746b1fad0014217cdb3befc7a0b9beb
artificial-composition-7121 <tbutler1132@gmail.com>
2020-07-20 16:13:35 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,23 @@
+def using_push(colors_in_the_rainbow, next_color)
+  colors_in_the_rainbow.push("violet")
+end
+
+def using_unshift(bouroughs_in_nyc, new_neighborhood)
+  bouroughs_in_nyc.unshift("Staten Island")
+end
+
+def using_pop(continents)
+  continents.pop
+end
+
+def pop_with_args(dog_breeds)
+  dog_breeds.pop(2)
+end
+
+def using_shift(my_favorite_classes)
+  my_favorite_classes.shift
+end
+
+def shift_with_args(ice_cream_brands)
+  ice_cream_brands.shift(2)
+end
